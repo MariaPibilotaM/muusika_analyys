7976bb49b99dac91341e6e15dc2595ae1dabcc01
Maria <mariamurumaaa@gmail.com>
2021-03-24 20:21:49 +0200

    Working app

--- app/app.py
+++ app/app.py
@@ -21,6 +21,7 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
+musicnn_metadata = json.load(open('msd-musicnn-1.json', 'r'))
 
 data = {}
 
@@ -53,8 +54,7 @@ def find_features(audio_path, name):
 
     musicnn_preds = es.TensorflowPredictMusiCNN(graphFilename='msd-musicnn-1.pb')(audio)
 
-    classes = ["rock", "pop", "alternative", "indie", "electronic", "female vocalists", "dance", "00s", "alternative rock", "jazz", "beautiful", "metal", "chillout", "male vocalists", "classic rock", "soul", "indie rock", "Mellow", "electronica", "80s", "folk", "90s", "chill", "instrumental", "punk", "oldies", "blues", "hard rock", "ambient", "acoustic", "experimental", "female vocalist", "guitar", "Hip-Hop", "70s", "party", "country", "easy listening", "sexy", "catchy", "funk", "electro", "heavy metal", "Progressive rock", "60s", "rnb", "indie pop", "sad", "House", "happy"],
-
+    classes = musicnn_metadata['classes']
 
     keskmised = np.mean(musicnn_preds.T, axis=1)
     valitud = []
